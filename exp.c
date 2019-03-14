b6df1f30242a43d31f0bef559eee3b1d39ee88b3
John <john@ankarstrom.se>
2019-03-14 22:00:19 +0100

    completely rewrote exp command
    (now supports $(command) expansions as well)

--- exp.c
+++ exp.c
@@ -1,143 +1,186 @@
 #include <err.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "shex.h"
 
-/* ---------------------------------------------------------------------
-`exp' execs into the following argv, but first having expanded any
-${variables} in it.
---------------------------------------------------------------------- */
-
-/***********************************************************************
-`append' appends character `chr' to a string `str' of size `*size',
-increasing the size if necessary.
-***********************************************************************/
-
-void append(char chr, char *str, size_t *size)
+/* return output of command described in `args' */
+char *command(char *args)
 {
-	size_t len, inc;
+	/* create argv from arg string */
+	char **ap, **argv;
+	int i, vlen;
 
-	len = strlen(str);
-	inc = 50;
+	vlen = 20;
+	if ((argv = reallocarray(NULL, vlen, sizeof (char *))) == NULL)
+		err(1, "reallocarray");
 
-	if (len + 1 > *size)
-		if ((str = realloc(str, *size + inc)) == NULL)
-			err(1, "realloc");
-	size += inc;
-	str[len] = chr;
-	str[len + 1] = '\0';
+	ap = argv;
+	i = 0;
+	while ((*ap = strsep(&args, " \t")) != NULL)
+		if (**ap != '\0') { // unless "empty" field
+			ap++;
+			i++;
+			if (i + 1 >= vlen) {
+				if ((argv = reallocarray(argv, vlen + 20, sizeof (char *))) == NULL)
+					err(1, "reallocarray");
+				vlen += 20;
+			}
+		}
+	*ap = NULL;
+
+	/* fork */
+	char *output;
+	int pipes[2];
+	int osize, r, written;
+	pid_t child_pid;
+
+	osize = 512;
+	if ((output = malloc(osize)) == NULL) err(1, "malloc");
+
+	if (pipe(pipes) == -1) err(1, "pipe");
+	if ((child_pid = fork()) == -1) err(1, "fork");
+
+	if (child_pid == 0) { // in child process
+		/* change stdout into write end of pipe */
+		if (dup2(pipes[1], STDOUT_FILENO) == -1) err(1, "dup2");
+		/* close now unneeded descriptors */
+		close(pipes[0]);
+		close(pipes[1]);
+		/* execute argv */
+		extern char **environ;
+		if (execvpe(argv[0], argv, environ) == -1) err(1, "execvpe");
+	} else { // in parent
+		close(pipes[1]);
+		written = 0;
+		while ((r = read(pipes[0], output, 512)) > 0) {
+			written += r;
+			if (written + 512 > osize) {
+				if ((output = realloc(output, osize + 512)) == NULL)
+					err(1, "realloc");
+				osize += 512;
+			}
+		}
+		if (r == -1) err(1, "read");
+
+		if (wait(NULL) == -1) err(1, "wait");
+	}
+
+	if (output[strlen(output) - 1] == '\n')
+		output[strlen(output) - 1] = '\0';
+
+	return output;
 }
 
-/***********************************************************************
-`variable_at' extracts and returns the variable name beginning at
-index `*i' in string `str'.  It changes `*i' to the index of the first
-character following the variable name.
-
-str[*i] should be the first character of the variable name, terminated
-by a single } character.
-
-`expand' also supports backslash-escaping of } and \.
-***********************************************************************/
-
-char *variable_at(char *str, size_t *i)
+/* return expansion of ${variable} or $(command) at index `i' in string `arg' */
+char *expand_at(char *arg, size_t *i)
 {
-	char *variable;
-	size_t size;
+	bool escape;
+	char *e, *pre;
+	enum {VARIABLE, COMMAND} type;
+	size_t start;
 
-	size = 10;
-	if ((variable = malloc(size)) == NULL)
-		err(1, "malloc");
+	*i += 1; // skip $
+	if (arg[*i] == '{') type = VARIABLE;
+	else if (arg[*i] == '(') type = COMMAND;
+	else goto syntaxerror;
+	*i += 1; // skip {/(
 
-	/* process variable name */
-	for (; *i < strlen(str); (*i)++) {
-		switch (str[*i]) {
-		/* escape */
+	escape = false;
+	start = *i;
+	for (; *i < strlen(arg); *i += 1) {
+		switch (arg[*i]) {
+		case '}':
+			if (type == VARIABLE && !escape) goto done;
+			break;
+		case ')':
+			if (type == COMMAND && !escape) goto done;
+			break;
 		case '\\':
-			(*i)++;
-			switch (str[*i]) {
-			/* special characters */
-			case '\\':
-			case '}':
-				append(str[*i], variable, &size);
-				break;
-			/* normal characters */
-			default:
-				append('\\', variable, &size);
-				append(str[*i], variable, &size);
+			if (!escape) {
+				escape = true;
+				continue;
+			}
+		}
+		escape = false;
+	}
+
+	goto enderror;
+
+done:
+	if ((pre = malloc(*i - start + 1)) == NULL) err(1, "malloc");
+	memcpy(pre, &arg[start], *i - start);
+	pre[*i - start + 1] = '\0';
+
+	if (type == VARIABLE) {
+		if ((e = getenv(pre)) == NULL) goto novariable;
+	} else {
+		e = command(pre);
+	}
+
+	return e;
+
+syntaxerror:
+	fprintf(stderr, "syntax error: no { or ( following $\n");
+	exit(1);
+enderror:
+	fprintf(stderr, "syntax error: $ expansion missing terminating bracket\n");
+	exit(1);
+novariable:
+	fprintf(stderr, "no such variable %s\n", pre);
+	exit(1);
+}
+
+/* expand all variables and commands in string `arg' and return the new string */
+char *expand_all(char *arg)
+{
+	bool escape;
+	char *r, *e;
+	size_t i, rsize, resize;
+
+	rsize = strlen(arg) + 1;
+	if ((r = malloc(rsize)) == NULL) err(1, "malloc");
+
+	escape = false;
+	for (i = 0; i < strlen(arg); i++) {
+		if (strlen(r) + 1 > rsize - 1) {
+			if ((r = realloc(r, rsize + 50)) == NULL) err(1, "realloc");
+			rsize += 50;
+		}
+
+		switch (arg[i]) {
+		case '$':
+			if (escape) goto append;
+			else {
+				e = expand_at(arg, &i);
+				resize = strlen(r) + strlen(e) + 1;
+				if (resize > rsize) {
+					if ((r = realloc(r, resize)) == NULL)
+						err(1, "realloc");
+					rsize = resize;
+				}
+				strlcat(r, e, resize);
 			}
 			break;
-		/* end */
-		case '}':
-			goto end;
-		/* normal character */
+		case '\\': // escape
+			if (escape) goto append;
+			else {
+				escape = true;
+				continue;
+			}
+			break;
 		default:
-			append(str[*i], variable, &size);
+		append:
+			r[strlen(r)] = arg[i];
+			r[strlen(r)] = '\0';
+			escape = false;
 		}
 	}
 
-	/* normally, } should take us to the `end' label */
-	fprintf(stderr, "(exp) syntax error: missing }\n");
-	exit(1);
-
-end:
-	return variable;
-}
-
-/***********************************************************************
-`expand' processes string `old' and expands any ${variables}, returning
-the resulting string `new'.
-
-It also supports backslash-escaping of $ and \.
-***********************************************************************/
-
-char *expand(char *old)
-{
-	char *new, *variable, *value;
-	size_t i, size;
-
-	size = strlen(old) + 1;
-	if ((new = malloc(size)) == NULL)
-		err(1, "malloc");
-
-	for (i = 0; i < strlen(old); i++) {
-		switch (old[i]) {
-		/* escape */
-		case '\\':
-			i++;
-			switch (old[i]) {
-			/* end of string */
-			case '\0':
-				append('\\', new, &size);
-				break;
-			/* special characters */
-			case '\\':
-			case '$':
-				append(old[i], new, &size);
-				break;
-			/* non-special characters */
-			default:
-				append('\\', new, &size);
-				append(old[i], new, &size);
-			}
-			break;
-		/* expand variable */
-		case '$':
-			if (old[i + 1] == '{') {
-				i += 2;
-				variable = variable_at(old, &i);
-				if ((value = getenv(variable)) == NULL) break;
-				strlcat(new, value, strlen(new) + strlen(value) + 1);
-				free(variable);
-				break;
-			}
-		/* normal character */
-		default:
-			append(old[i], new, &size);
-		}
-	};
-
-	return new;
+	return r;
 }
 
 int main(int argc, char *argv[])
@@ -145,13 +188,10 @@ int main(int argc, char *argv[])
 	int c, i;
 
 	c = 1;
-	while (!terminator(argv[c])) c++; // expand until terminator (semicolon)
+	while (!terminator(argv[c])) c++;
 
-	if (c < 2) usage("%s argv ...", argv[0]);
-
-	/* expand arguments within every argument separately */
 	for (i = 1; i < c; i++) {
-		argv[i] = expand(argv[i]);
+		argv[i] = expand_all(argv[i]);
 	}
 
 	execat(argc, argv, 1);
